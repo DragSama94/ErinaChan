54934266e2e632b1cd8ba3a440d93f781b0921b8
MrBhavya <51018664+MrBhavya@users.noreply.github.com>
2020-10-03 12:16:54 +0000

    Update __main__.py

--- alluka/__main__.py
+++ alluka/__main__.py
@@ -145,7 +145,7 @@ def send_start(bot, update):
     text += ""  
 
     keyboard = [[InlineKeyboardButton(text="❓ Help", callback_data="help_back"),InlineKeyboardButton(text="⚙️ Connect Group", callback_data="main_connect")]]
-    keyboard += [[InlineKeyboardButton(text="🔌 Add me",url="http://t.me/zoldycktmbot?startgroup=true"),InlineKeyboardButton(text="👤 Support chat",url="https://telegram.dog/NakiriSupportGroup")]]
+    keyboard += [[InlineKeyboardButton(text="🔌 Add me",url="http://t.me/erinachanbot?startgroup=true"),InlineKeyboardButton(text="👤 Support chat",url="https://telegram.dog/NakiriSupportGroup")]]
 
     update.effective_message.reply_photo(HELP_PANEL_STRING.ALLUKA_IMG,text, reply_markup=InlineKeyboardMarkup(keyboard), parse_mode=ParseMode.MARKDOWN)
 
